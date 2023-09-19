ed5bfbdd058f32f51e74bd85d7dd74e4d867e54b
Saul Muñoz <sam.pedreros14@gmail.com>
2023-09-19 02:35:25 -0300

    All Works

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,22 +1,57 @@
 import React, { useState } from 'react';
-import { Button, Card, CardContent, CardHeader, Divider, Paper, IconButton, Grid } from '@mui/material';
+import { Button, Card, CardContent, CardHeader, Divider, Paper, IconButton, Box } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-import { DownloadOutlined, MergeRounded } from '@mui/icons-material';
-import MarkdownPreview from '@uiw/react-markdown-preview';
+import { DownloadOutlined } from '@mui/icons-material';
 import './LandingPage.css';
+import MarkdownPreview from '@uiw/react-markdown-preview';
+import { MergeRounded } from '@mui/icons-material';
 
-// Componente para mostrar la vista previa de Markdown fusionado
-const MergedMarkdownPreview = ({ markdown }) => {
+
+// Nuevo componente para la sección de previsualización
+function PreviewSection({ mergedMarkdown, onDownloadClick, onBackClick }) {
     return (
-        <div>
-            <MarkdownPreview source={markdown} />
+        <div className="markdown-preview-container">
+            <Card>
+                <CardHeader
+                    title="Vista Previa del Markdown Fusionado"
+                    sx={{ backgroundColor: '#1976D2', color: 'white' }}
+                />
+                <Divider />
+                <CardContent>
+                    <Paper style={{ maxHeight: '500px', overflow: 'auto' }}>
+                        {/* Utiliza el componente MarkdownPreview para mostrar la vista previa */}
+                        <MarkdownPreview source={mergedMarkdown} />
+                    </Paper>
+                </CardContent>
+                <CardContent>
+                    {/* Botones en la sección de previsualización */}
+                    <Button
+                        variant="contained"
+                        startIcon={<DownloadOutlined />}
+                        color="primary"
+                        onClick={onDownloadClick}
+                        sx={{ margin: '8px' }}
+                    >
+                        Descargar
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        color="primary"
+                        onClick={onBackClick}
+                        sx={{ margin: '8px' }}
+                    >
+                        Volver para fusionar nuevos archivos
+                    </Button>
+                </CardContent>
+            </Card>
         </div>
     );
-};
+}
 
 function LandingPage() {
     const [selectedFiles, setSelectedFiles] = useState([]);
     const [mergedMarkdown, setMergedMarkdown] = useState('');
+    const [showPreview, setShowPreview] = useState(false); // Estado para mostrar la previsualización
 
     // Función para manejar la selección de archivos
     const handleFileSelect = (event) => {
@@ -56,6 +91,7 @@ function LandingPage() {
             } else {
                 // Todos los archivos han sido leídos y fusionados
                 setMergedMarkdown(mergedMarkdown);
+                setShowPreview(true); // Mostrar la previsualización después de la fusión
             }
         };
 
@@ -82,54 +118,68 @@ function LandingPage() {
         a.download = 'merged.md'; // Establece el nombre de archivo deseado aquí
 
         // Simular un clic en el elemento <a> para iniciar la descarga
-        document.body.appendChild(a);
         a.click();
 
         // Revocar la URL para liberar recursos
         window.URL.revokeObjectURL(url);
-        document.body.removeChild(a);
+    };
+
+    // Función para volver atrás y fusionar nuevos archivos
+    const handleBackClick = () => {
+        setShowPreview(false); // Ocultar la previsualización y mostrar el área de fusión nuevamente
+        setSelectedFiles([]);
+        setMergedMarkdown('');
     };
 
     return (
         <div className="landing-page">
-            <Grid container spacing={2}>
-                {/* Sección de Drag and Drop (Izquierda) */}
-                <Grid item xs={12} sm={6}>
-                    <Paper className="markdown-joiner">
-                        <input type="file" id="file-input" multiple accept=".md" style={{ display: 'none' }} onChange={handleFileSelect} />
+            <Box
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="center"
+                minHeight="80vh"
+            >
+                {showPreview ? (
+                    // Mostrar la sección de previsualización si showPreview es true
+                    <PreviewSection
+                        mergedMarkdown={mergedMarkdown}
+                        onDownloadClick={handleDownloadClick}
+                        onBackClick={handleBackClick}
+                    />
+                ) : (
+                    // Mostrar el área de "Fusionar Archivos" y "Drag and Drop" si showPreview es false
+                    <div className="markdown-joiner">
+                        <input
+                            type="file"
+                            id="file-input"
+                            multiple
+                            accept=".md"
+                            style={{ display: 'none' }}
+                            onChange={handleFileSelect}
+                        />
                         <label htmlFor="file-input">
-                            {/* Zona de arrastre o botón de carga */}
-                            <IconButton color="primary" aria-label="upload file" component="span">
+                            <IconButton
+                                color="primary"
+                                aria-label="upload file"
+                                component="span"
+                            >
                                 <CloudUploadIcon />
                             </IconButton>
                             Selecciona o Arrastra tus archivos
                         </label>
-                        <Button variant="outlined" startIcon={<MergeRounded />} color="primary" onClick={handleMergeClick}>
+                        <Button
+                            variant="outlined"
+                            startIcon={<MergeRounded />}
+                            color="primary"
+                            onClick={handleMergeClick}
+                            sx={{ marginTop: '4px', margin: '8px' }}
+                        >
                             Fusionar Archivos
                         </Button>
-                        <Button variant="contained" startIcon={<DownloadOutlined />} color="primary" onClick={handleDownloadClick}>
-                            Descargar
-                        </Button>
-                    </Paper>
-                </Grid>
-
-                {/* Columna Vertical a la Derecha (Visualización de Markdown) */}
-                <Grid item xs={12} sm={6}>
-                    <Card sx={{ maxWidth: 800, margin: 'auto', boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.1)', background: 'white' }}>
-                        <CardHeader
-                            title="Vista Previa del Markdown Fusionado"
-                            sx={{ backgroundColor: '#1976D2', color: 'white' }}
-                        />
-                        <Divider />
-                        <CardContent sx={{ backgroundColor: 'white', padding: '0px' }}>
-                            <Paper style={{ maxHeight: '500px', overflowY: 'auto' }} className='markdown-preview-container'>
-                                {/* Utiliza el componente MergedMarkdownPreview para mostrar la vista previa */}
-                                <MergedMarkdownPreview markdown={mergedMarkdown} />
-                            </Paper>
-                        </CardContent>
-                    </Card>
-                </Grid>
-            </Grid>
+                    </div>
+                )}
+            </Box>
         </div>
     );
 }
