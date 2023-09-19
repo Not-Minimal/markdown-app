d9f2c4dffca1e5e1360326ec18bcfbec236969eb
Saul Muñoz <sam.pedreros14@gmail.com>
2023-09-19 17:10:59 -0300

    Temas Actualizado

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,7 @@
 // theme.js
 import { createTheme } from '@mui/material/styles';
 
+// Tema claro
 const theme = createTheme({
     spacing: 4,
     typography: {
@@ -10,53 +11,78 @@ const theme = createTheme({
             'Open Sans',
         ].join(','),
         h1: {
-            fontSize: '5rem',
+            fontSize: '2.5rem', // Ajusta el tamaño de la fuente para h1
             fontFamily: 'Raleway',
+            fontWeight: 'bold', // Añade negrita
         },
         h2: {
-            fontSize: '3.5rem',
+            fontSize: '2rem', // Ajusta el tamaño de la fuente para h2
             fontFamily: 'Open Sans',
-            fontStyle: 'bold',
+            fontWeight: 'bold', // Añade negrita
         },
         h3: {
-            fontSize: '2.5rem',
+            fontSize: '1.5rem', // Ajusta el tamaño de la fuente para h3
             fontFamily: 'Roboto',
+            fontWeight: 'bold', // Añade negrita
         },
     },
     palette: {
         background: {
-            default: '#009900'//green
+            default: '#FFFFFF', // Fondo blanco
         },
         primary: {
-            main: '#2B37D4',//indigo
+            main: '#1976D2', // Color principal azul
         },
         secondary: {
-            main: '#E769A6',//pink
+            main: '#FF5722', // Color secundario naranja
         },
         error: {
-            main: '#D72A2A',//red
+            main: '#D32F2F', // Color de error rojo
         },
         warning: {
-            main: '#FC7B09',//orange
+            main: '#FFA000', // Color de advertencia naranja
         },
         info: {
-            main: '#6B7D6A',//gray
+            main: '#1976D2', // Color de información azul (coincide con el color principal)
         },
         success: {
-            main: '#09FE00',//green
+            main: '#388E3C', // Color de éxito verde
         },
         text: {
-            primary: '#000000',//black
-            secondary: '#FFFFFF',//white
+            primary: '#000000', // Texto negro en lugar de negro puro
+            secondary: '#757575', // Texto secundario gris oscuro
         },
     },
-
 });
 
+// Tema oscuro
 const themeDark = createTheme({
-    darkMode: {
+    spacing: 4,
+    typography: {
+        fontFamily: [
+            'Roboto',
+            'Raleway',
+            'Open Sans',
+        ].join(','),
+        h1: {
+            fontSize: '2.5rem',
+            fontFamily: 'Raleway',
+            fontWeight: 'bold',
+        },
+        h2: {
+            fontSize: '2rem',
+            fontFamily: 'Open Sans',
+            fontWeight: 'bold',
+        },
+        h3: {
+            fontSize: '1.5rem',
+            fontFamily: 'Roboto',
+            fontWeight: 'bold',
+        },
+    },
+    palette: {
         background: {
-            default: '#121212', // Color de fondo oscuro
+            default: '#121212', // Fondo oscuro
         },
         primary: {
             main: '#64B5F6', // Color principal en modo oscuro
@@ -64,12 +90,23 @@ const themeDark = createTheme({
         secondary: {
             main: '#FFB74D', // Color secundario en modo oscuro
         },
+        error: {
+            main: '#EF5350', // Color de error en modo oscuro (rojo menos intenso)
+        },
+        warning: {
+            main: '#FFA000', // Color de advertencia en modo oscuro (naranja menos intenso)
+        },
+        info: {
+            main: '#64B5F6', // Color de información en modo oscuro (coincide con el color principal)
+        },
+        success: {
+            main: '#4CAF50', // Color de éxito en modo oscuro (verde menos intenso)
+        },
         text: {
-            primary: '#FFFFFF', // Color del texto en modo oscuro
-            secondary: '#B0B0B0', // Color del texto secundario en modo oscuro
+            primary: '#FFFFFF', // Texto blanco en modo oscuro
+            secondary: '#B0B0B0', // Texto secundario gris en modo oscuro
         },
     },
 });
 
 export { theme, themeDark };
-
