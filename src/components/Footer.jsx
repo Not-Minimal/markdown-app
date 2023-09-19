6ae1b571baf5e6b7fe54433db5730a0878aad929
Saul Muñoz <sam.pedreros14@gmail.com>
2023-09-19 18:42:15 -0300

    Todo funciona Bine

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,80 +1,35 @@
 'use client';
 
-import React from 'react';
 import { Footer } from 'flowbite-react';
-import { BsGithub, BsInstagram } from 'react-icons/bs';
-import './Footer.css';
 
+const footerStyle = {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+};
 
-export default function FooterWithSocialMediaIcons() {
+export default function DefaultFooter() {
     return (
-        <Footer container className="footer-container">
-            <div className="w-full">
-                <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
-                    <div>
-                        <Footer.Brand
-                            alt="Flowbite Logo"
-                            href="https://flowbite.com"
-                            name="Markdown Merger"
-                            src="https://flowbite.com/docs/images/logo.svg"
-                        />
-                    </div>
-                    <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
-                        <div>
-                            <Footer.Title title="about" />
-                            <Footer.LinkGroup col>
-                                <Footer.Link href="#">
-                                    Flowbite
-                                </Footer.Link>
-                                <Footer.Link href="#">
-                                    Tailwind CSS
-                                </Footer.Link>
-                            </Footer.LinkGroup>
-                        </div>
-                        <div>
-                            <Footer.Title title="Follow us" />
-                            <Footer.LinkGroup col>
-                                <Footer.Link href="https://youtube.com/@NotMinimal">
-                                    Youtube
-                                </Footer.Link>
-                                <Footer.Link href="https://github.com/Not-Minimal">
-                                    GitHub
-                                </Footer.Link>
-                            </Footer.LinkGroup>
-                        </div>
-                        <div>
-                            <Footer.Title title="Legal" />
-                            <Footer.LinkGroup col>
-                                <Footer.Link href="#">
-                                    Privacy Policy
-                                </Footer.Link>
-                                <Footer.Link href="#">
-                                    Terms & Conditions
-                                </Footer.Link>
-                            </Footer.LinkGroup>
-                        </div>
-                    </div>
-                </div>
-                <Footer.Divider />
-                <div className="w-full sm:flex sm:items-center sm:justify-between">
-                    <Footer.Copyright
-                        by="Minimal Services"
-                        href="#"
-                        year={2023}
-                    />
-                    <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-                        <Footer.Icon
-                            href="https://www.instagram.com/nottminimal/"
-                            icon={BsInstagram}
-                        />
-                        <Footer.Icon
-                            href="https://github.com/Not-Minimal"
-                            icon={BsGithub}
-                        />
-
-                    </div>
-                </div>
-            </div>
+        <Footer container style={footerStyle}>
+            <Footer.Copyright
+                by="MergeMarkdown"
+                href="#"
+                year={2023}
+            />
+            <Footer.LinkGroup>
+                <Footer.Link href="#">
+                    About
+                </Footer.Link>
+                <Footer.Link href="#">
+                    Privacy Policy
+                </Footer.Link>
+                <Footer.Link href="#">
+                    Licensing
+                </Footer.Link>
+                <Footer.Link href="#">
+                    Contact
+                </Footer.Link>
+            </Footer.LinkGroup>
         </Footer>
-    )
+    );
 }
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,21 +1,20 @@
+// LandingPage.js
+
 import React, { useState } from 'react';
-import { Button, Card, CardContent, CardHeader, Divider, Paper, IconButton, Box } from '@mui/material';
+import { Button, Card, CardContent, CardHeader, Divider, Paper, IconButton } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-import { MergeRounded, DownloadOutlined } from '@mui/icons-material';
+import { DownloadOutlined } from '@mui/icons-material';
 import MarkdownPreview from '@uiw/react-markdown-preview';
-import { useTheme } from '@mui/material/styles'; // Importa useTheme para acceder al tema
+import { MergeRounded } from '@mui/icons-material';
 import './LandingPage.css';
 
-// Nuevo componente para la sección de previsualización
 function PreviewSection({ mergedMarkdown, onDownloadClick, onBackClick }) {
-    const theme = useTheme(); // Obtiene el tema actual
-
     return (
         <div className="markdown-preview-container">
             <Card>
                 <CardHeader
                     title="Vista Previa del Markdown Fusionado"
-                    sx={{ backgroundColor: theme.palette.primary.main, color: theme.palette.text.secondary }} // Usa colores del tema
+                    sx={{ backgroundColor: '#1976D2', color: 'white' }}
                 />
                 <Divider />
                 <CardContent>
@@ -50,106 +49,74 @@ function PreviewSection({ mergedMarkdown, onDownloadClick, onBackClick }) {
 function LandingPage() {
     const [selectedFiles, setSelectedFiles] = useState([]);
     const [mergedMarkdown, setMergedMarkdown] = useState('');
-    const [showPreview, setShowPreview] = useState(false); // Estado para mostrar la previsualización
+    const [showPreview, setShowPreview] = useState(false);
 
-    // Función para manejar la selección de archivos
     const handleFileSelect = (event) => {
         const files = Array.from(event.target.files);
-
-        // Filtrar archivos .md
         const markdownFiles = files.filter((file) => file.name.endsWith('.md'));
-
         setSelectedFiles(markdownFiles);
     };
 
-    // Función para fusionar y generar la salida
     const handleMergeClick = () => {
         if (selectedFiles.length === 0) {
             alert('Por favor, selecciona archivos Markdown para fusionar.');
             return;
         }
 
-        // Ordenar archivos alfabéticamente por nombre
         const sortedFiles = selectedFiles.slice().sort((a, b) => a.name.localeCompare(b.name));
-
-        // Inicializar un FileReader y una variable de salida
         const reader = new FileReader();
         let mergedMarkdown = '';
 
-        // Función para manejar la lectura de cada archivo
         const readNextFile = (index) => {
             if (index < sortedFiles.length) {
                 reader.onload = (e) => {
                     const fileContents = e.target.result;
                     mergedMarkdown += fileContents + '\n';
-
-                    // Leer el siguiente archivo
                     readNextFile(index + 1);
                 };
                 reader.readAsText(sortedFiles[index]);
             } else {
-                // Todos los archivos han sido leídos y fusionados
                 setMergedMarkdown(mergedMarkdown);
-                setShowPreview(true); // Mostrar la previsualización después de la fusión
+                setShowPreview(true);
             }
         };
 
-        // Comenzar a leer los archivos
         readNextFile(0);
     };
 
-    // Función para generar y descargar el archivo Markdown fusionado
     const handleDownloadClick = () => {
         if (!mergedMarkdown) {
             alert('Por favor, fusiona archivos Markdown primero.');
             return;
         }
 
-        // Crear un blob con el contenido Markdown
         const blob = new Blob([mergedMarkdown], { type: 'text/markdown' });
-
-        // Crear una URL para el blob
         const url = window.URL.createObjectURL(blob);
-
-        // Crear un elemento <a> para descargar
         const a = document.createElement('a');
         a.href = url;
-        a.download = 'merged.md'; // Establece el nombre de archivo deseado aquí
-
-        // Simular un clic en el elemento <a> para iniciar la descarga
+        a.download = 'merged.md';
         a.click();
-
-        // Revocar la URL para liberar recursos
         window.URL.revokeObjectURL(url);
     };
 
-    // Función para volver atrás y fusionar nuevos archivos
     const handleBackClick = () => {
-        setShowPreview(false); // Ocultar la previsualización y mostrar el área de fusión nuevamente
+        setShowPreview(false);
         setSelectedFiles([]);
         setMergedMarkdown('');
     };
 
     return (
-        <div className="landing-page">
-            <Box
-                display="flex"
-                flexDirection="column"
-                alignItems="center"
-                justifyContent="center"
-                minHeight="80vh"
-            >
-                {showPreview ? (
-                    // Mostrar la sección de previsualización si showPreview es true
-                    <PreviewSection
-                        mergedMarkdown={mergedMarkdown}
-                        onDownloadClick={handleDownloadClick}
-                        onBackClick={handleBackClick}
-                    />
-                ) : (
-                    // Mostrar el área de "Fusionar Archivos" y "Drag and Drop" si showPreview es false
+        <div className="landing-page-container">
+            {showPreview ? (
+                <PreviewSection
+                    mergedMarkdown={mergedMarkdown}
+                    onDownloadClick={handleDownloadClick}
+                    onBackClick={handleBackClick}
+                />
+            ) : (
+                <div className="landing-content">
                     <div className="markdown-joiner">
-                        <div style={{ marginBottom: '16px' }}>
+                        <div style={{ marginBottom: '16px', padding: '20px' }}>
                             <input
                                 type="file"
                                 id="file-input"
@@ -166,7 +133,7 @@ function LandingPage() {
                                 >
                                     <CloudUploadIcon />
                                 </IconButton>
-                                Selecciona o Arrastra tus archivos
+                                Seleccionar Archivos
                             </label>
                         </div>
                         <div>
@@ -177,12 +144,12 @@ function LandingPage() {
                                 onClick={handleMergeClick}
                                 sx={{ margin: '8px' }}
                             >
-                                Fusionar Archivos
+                                Unir Archivos
                             </Button>
                         </div>
                     </div>
-                )}
-            </Box>
+                </div>
+            )}
         </div>
     );
 }
